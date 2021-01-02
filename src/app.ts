f1f8e27b26be930f81b29630349c1ed55010d82d
masafumi kato <xslash1105@gmail.com>
2021-01-02 23:48:03 +0900

    [update] interface

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,34 @@
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
-  titleInputElement: HTMLInputElement;
-  descriptionInputElement: HTMLInputElement;
-  peopleInputElement: HTMLInputElement;
+/// <reference path="drag-drop-interfaces.ts" />
 
-  constructor() {
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+namespace DDInterfaces {
+  class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
-    const importedNode = document.importNode(this.templateElement.content, true);
-    this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = 'user-input';
+    constructor() {
+      this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+      this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
-    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+      const importedNode = document.importNode(this.templateElement.content, true);
+      this.element = importedNode.firstElementChild as HTMLFormElement;
+      this.element.id = 'user-input';
 
-    this.attach();
+      this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+      this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+      this.attach();
+    }
+
+    private attach() {
+      this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
   }
 
-  private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
-  }
+  const prjInput = new ProjectInput();
 }
 
-const prjInput = new ProjectInput();
---
+++ src/drag-drop-interfaces.ts
@@ -0,0 +1,12 @@
+namespace DDInterfaces {
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+  }
+
+  export interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dragHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+  }
+}
